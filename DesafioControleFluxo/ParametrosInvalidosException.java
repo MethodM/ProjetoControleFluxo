47746ce53adf3e1daa924ef93ba4cd690ee09783
MethodM <casagrande.theus@gmail.com>
2025-05-17 01:11:14 -0300

    Classe Contador adicionada para efetuar a interação do código

---
+++ DesafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package DesafioControleFluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+    }
+}
